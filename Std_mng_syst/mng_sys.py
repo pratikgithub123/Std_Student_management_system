6041ddb37490aa36b73ea79f0f75ce5dd100b904
pratikgithub123 <dangipratik19@gmail.com>
2021-10-04 14:40:42 +0545

    table created

--- Std_mng_syst/mng_sys.py
+++ Std_mng_syst/mng_sys.py
@@ -6,3 +6,30 @@ from tkinter import messagebox
 
 
 
+def login_page():
+
+
+
+
+#Databases
+
+    # Create a databases or connect to one
+    conn = sqlite3.connect('address_book.db')
+
+    # Create cursor
+    c = conn.cursor()
+
+    '''
+    # Create table
+    c.execute(""" CREATE TABLE addresses(
+          name text,
+          fathers_name text,
+          mothers_name text,
+          address text,
+          gender text,
+          date_of_birth text,
+          phone_num integer
+    ) """)
+    '''
+    # Create submit button for databases
+
