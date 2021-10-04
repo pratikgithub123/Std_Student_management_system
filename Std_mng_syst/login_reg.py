4b1f0ebc33bd3eeb6dfe1fa312cf6b7364d28c2d
pratikgithub123 <dangipratik19@gmail.com>
2021-10-04 14:55:25 +0545

    last code

--- Std_mng_syst/login_reg.py
+++ Std_mng_syst/login_reg.py
@@ -12,7 +12,7 @@ def register():
     global register_screen
     register_screen = Toplevel(main_screen)
     register_screen.title("Register")
-    register_screen.geometry("300x250")
+    register_screen.geometry("310x250")
     register_screen.configure(bg="skyblue")
     register_screen.iconbitmap("C:/Users/hp/PycharmProjects/Std_mng_syst/Std_mng_syst/register.ico")
 
@@ -41,7 +41,7 @@ def login():
     global login_screen
     login_screen = Toplevel(main_screen)
     login_screen.title("Login")
-    login_screen.geometry("300x250")
+    login_screen.geometry("310x250")
     login_screen.configure(bg="lightblue")
     login_screen.iconbitmap("C:/Users/hp/PycharmProjects/Std_mng_syst/Std_mng_syst/data.ico")
 
@@ -70,6 +70,73 @@ def login():
     password_login_entry.pack()
     Label(login_screen, text="").pack()
     Button(login_screen, text="Login",bg="skyblue", width=10, height=1, font=("Times", "12", "bold"),command=login_verify).pack()
+
+def register_user():
+    username_info = username.get()
+    password_info = password.get()
+
+    file = open(username_info, "w")
+    file.write(username_info + "\n")
+    file.write(password_info)
+    file.close()
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    messagebox.showinfo("Success","Registration Success")
+
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_login_entry.delete(0, END)
+    password_login_entry.delete(0, END)
+
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+
+        else:
+            password_not_recognised()
+
+    else:
+        user_not_found()
+
+#popup for login sucess
+
+def login_sucess():
+    global login_success_screen
+    messagebox.showinfo("Login","Login Success")
+
+    login_page()
+
+
+#popup for password not recognised
+
+def password_not_recognised():
+    global password_not_recognised_screen
+    messagebox.showinfo("Incorrect", "Password Not Recognised")
+
+# popup for user not found
+
+def user_not_found():
+    global user_not_found_screen
+    messagebox.showinfo("Not found","User not found")
+
+# clearing popup
+
+def delete_login_success():
+    login_success_screen.destroy()
+
+
+def delete_password_not_recognised():
+    password_not_recognised_screen.destroy()
+
+
+def delete_user_not_found():
+    user_not_found_screen.destroy()
 
 
 
