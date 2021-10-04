4bf6237fd618d674caebbf7833e6df7bd4ab2b5d
pratikgithub123 <dangipratik19@gmail.com>
2021-10-04 14:44:06 +0545

    query and delete function added

--- Std_mng_syst/mng_sys.py
+++ Std_mng_syst/mng_sys.py
@@ -67,3 +67,70 @@ def login_page():
         date_of_birth.delete(0, END)
         phone_num.delete(0, END)
 
+    def query():
+        # Create a databases or connect to one
+        conn = sqlite3.connect('address_book.db')
+
+        # Create cursor
+        c = conn.cursor()
+
+        # query of the database
+        c.execute("SELECT *, oid FROM addresses")
+
+        records = c.fetchall()
+       # print(records)
+
+        # Loop through the results
+        print_record=''
+        for record in records:
+            #str(record[6]) added for displaying the id
+            print_record += str(record[0]) + ' ' + str(record[1]) + ' ' + str(record[2]) + ' ' + str(record[3]) + ' ' + str(
+                record[4]) + ' ' + str(record[5]) +  ' '+str(record[6]) + '    '+str(record[7])+ "\n"
+
+        print("showing data", print_record)
+        query_label = Label(details_frame, text=print_record, font=("Times", 10, "bold"))
+        query_label.place(x=0, y=0)
+
+
+
+
+        conn.commit()
+        conn.close()
+
+
+
+
+
+    # Creating a function to delete a record
+
+    def delete():
+        # create database
+        conn = sqlite3.connect('address_book.db')
+
+        #create cursor
+        c = conn.cursor()
+
+        #delete a record
+        c.execute("DELETE from addresses WHERE oid = " + delete_box.get())
+        print('Deleted Successfully')
+
+        # query of the database
+        c.execute("SELECT *, oid FROM addresses")
+
+        records = c.fetchall()
+        # print(records)
+
+        # Loop through the results
+        print_record = ''
+        for record in records:
+            # str(record[6]) added for displaying the id
+            print_record += str(record[0]) + ' ' + str(record[1]) + ' ' + str(record[2]) + ' ' + str(record[3]) + ' ' + str(
+                record[4]) + ' ' + str(record[5]) + ' ' + str(record[6]) +' ' +str(record[7])+ "\n"
+
+        delete_box.delete(0, END)
+        conn.commit()
+        conn.close()
+        messagebox.showinfo("Deleted successfully", "Data Deleted sucessfully")
+
+
+    # Create edit function to update a record
