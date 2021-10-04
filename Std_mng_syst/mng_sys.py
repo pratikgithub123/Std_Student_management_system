2b5770bcb8b3b21d6d067201aa590d1bc0e1b9b8
pratikgithub123 <dangipratik19@gmail.com>
2021-10-04 20:38:32 +0545

    final code

--- Std_mng_syst/mng_sys.py
+++ Std_mng_syst/mng_sys.py
@@ -40,13 +40,13 @@ def login_page():
         c.execute(
             "INSERT INTO addresses VALUES (:name, :fathers_name, :mothers_name, :address, :gender, :date_of_birth,:phone_num)",
             {
-                'name': name.get(),
-                'fathers_name': fathers_name.get(),
-                'mothers_name': mothers_name.get(),
-                'address': address.get(),
-                'gender': gender.get(),
-                'date_of_birth': date_of_birth.get(),
-                'phone_num': phone_num.get()
+                'name': Name_entry.get(),
+                'fathers_name': FathersName_entry.get(),
+                'mothers_name': MothersName_entry.get(),
+                'address': Address_entry.get(),
+                'gender': Gender_entry.get(),
+                'date_of_birth': DOB_entry.get(),
+                'phone_num': PhoneNum_entry.get()
             })
         # showinfo messagebox
         messagebox.showinfo("Adresses", "Inserted Successfully")
@@ -56,13 +56,13 @@ def login_page():
         conn.close()
 
         # clear the text boxes
-        name.delete(0, END)
-        fathers_name.delete(0, END)
-        mothers_name.delete(0, END)
-        address.delete(0, END)
-        gender.delete(0, END)
-        date_of_birth.delete(0, END)
-        phone_num.delete(0, END)
+        Name_entry.delete(0, END)
+        FathersName_entry.delete(0, END)
+        MothersName_entry.delete(0, END)
+        Address_entry.delete(0, END)
+        Gender_entry.delete(0, END)
+        DOB_entry.delete(0, END)
+        PhoneNum_entry.delete(0, END)
 
     def query():
         # Create a databases or connect to one
@@ -102,27 +102,12 @@ def login_page():
         c = conn.cursor()
 
         # delete a record
-        c.execute("DELETE from addresses WHERE oid = " + delete_box.get())
+        c.execute("DELETE from addresses WHERE oid = " + clear_box.get())
         print('Deleted Successfully')
-
-        # query of the database
-        c.execute("SELECT *, oid FROM addresses")
-
-        records = c.fetchall()
-        # print(records)
-
-        # Loop through the results
-        print_record = ''
-        for record in records:
-            # str(record[6]) added for displaying the id
-            print_record += str(record[0]) + ' ' + str(record[1]) + ' ' + str(record[2]) + ' ' + str(
-                record[3]) + ' ' + str(
-                record[4]) + ' ' + str(record[5]) + ' ' + str(record[6]) + ' ' + str(record[7]) + "\n"
-
-        delete_box.delete(0, END)
+        clear_box.delete(0,END)
         conn.commit()
         conn.close()
-        messagebox.showinfo("Deleted successfully", "Data Deleted sucessfully")
+
 
     # Create edit function to update a record
     def edit():
@@ -141,7 +126,7 @@ def login_page():
         # Create cursor
         c = conn.cursor()
 
-        record_id = delete_box.get()
+        record_id = clear_box.get()
 
         # query of the database
         c.execute("SELECT * FROM addresses WHERE oid=" + record_id)
@@ -159,6 +144,46 @@ def login_page():
         global phone_num_editor
 
         # Create text boxes
+
+
+    # Creating an update function
+        def update():
+        # Create a databases or connect to one
+            conn = sqlite3.connect('address_book.db')
+
+        # Create cursor
+            c = conn.cursor()
+
+            record_id = clear_box.get()
+
+            c.execute("""UPDATE addresses SET
+                 name=:nam,
+                 fathers_name=:fathers_n,
+                 mothers_name=:mothers_n,
+                 address=:addr,
+                 gender=:gend,
+                 date_of_birth=:date_of,
+                 phone_num=:phon
+    
+                 WHERE oid =:oid""",
+                      {'nam': name_editor.get(),
+                       'fathers_n': fathers_name_editor.get(),
+                       'mothers_n': mothers_name_editor.get(),
+                       'addr': address_editor.get(),
+                       'gend': gender_editor.get(),
+                       'date_of': date_of_birth_editor.get(),
+                       'phon': phone_num_editor.get(),
+                       'oid': record_id
+                       }
+                      )
+
+            conn.commit()
+            conn.close()
+
+            messagebox.showinfo("Updated sucessfully", "YOUR DATA HAVE BEEN SUCCESSFULLY UPDATED")
+
+            editor.destroy()
+
         name_editor = Entry(editor, font=("Times", 15, "bold"), bg="white", width=30)
         name_editor.grid(row=0, column=1, padx=3, pady=5)
 
@@ -172,13 +197,13 @@ def login_page():
         address_editor.grid(row=3, column=1, padx=3, pady=5)
 
         gender_editor = Entry(editor, font=("Times", 15, "bold"), bg="white", width=30)
-        gender_editor.grid(row=3, column=1, padx=3, pady=5)
+        gender_editor.grid(row=4, column=1, padx=3, pady=5)
 
         date_of_birth_editor = Entry(editor, font=("Times", 15, "bold"), bg="white", width=30)
-        date_of_birth_editor.grid(row=4, column=1, padx=3, pady=5)
+        date_of_birth_editor.grid(row=5, column=1, padx=3, pady=5)
 
         phone_num_editor = Entry(editor, font=("Times", 15, "bold"), bg="white", width=30)
-        phone_num_editor.grid(row=5, column=1, padx=3, pady=5)
+        phone_num_editor.grid(row=6, column=1, padx=3, pady=5)
 
         # Create textbox labels
         name_label = Label(editor, font=("Times", 15, "bold"), bg="lightblue", text="Students Name")
@@ -194,13 +219,13 @@ def login_page():
         address_label.grid(row=3, column=0, padx=3, pady=5)
 
         gender_label = Label(editor, font=("Times", 15, "bold"), bg="lightblue", text="Gender")
-        gender_label.grid(row=3, column=0, padx=3, pady=5)
+        gender_label.grid(row=4, column=0, padx=3, pady=5)
 
         date_of_birth_label = Label(editor, font=("Times", 15, "bold"), bg="lightblue", text="DOB")
-        date_of_birth_label.grid(row=4, column=0, padx=3, pady=5)
+        date_of_birth_label.grid(row=5, column=0, padx=3, pady=5)
 
         phone_num_label = Label(editor, font=("Times", 15, "bold"), bg="lightblue", text="Phone No")
-        phone_num_label.grid(row=5, column=0, padx=3, pady=5)
+        phone_num_label.grid(row=6, column=0, padx=3, pady=5)
 
         # loop
         for record in records:
@@ -212,46 +237,11 @@ def login_page():
             date_of_birth_editor.insert(0, record[5])
             phone_num_editor.insert(0, record[6])
 
-        # Create a update button
+    # Create a update button
         edit_btn = Button(editor, font=("Times", 15, "bold"), bg="lightblue", text=" SAVE ", command=update)
         edit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=125)
 
-    # Creating an update function
-    def update():
-        # Create a databases or connect to one
-        conn = sqlite3.connect('address_book.db')
-
-        # Create cursor
-        c = conn.cursor()
-
-        record_id = delete_box.get()
-
-        c.execute("""UPDATE addresses SET
-            name=:name,
-            fathers_name=:fathers_name,
-            mothers_name=:mothers_name,
-            address=:address,
-            gender=:gender,
-            date_of_birth=:date_of_birth,
-            phone_num=:phone_num
-
-            WHERE oid =:oid""",
-                  {'name': name_editor.get(),
-                   'fathers_name': fathers_name.get(),
-                   'mothers_name': mothers_name.get(),
-                   'address': address.get(),
-                   'gender': gender.get(),
-                   'date_of_birth': date_of_birth.get(),
-                   'phone_num': phone_num.get(),
-                   'oid': record_id
-                   })
-
-        conn.commit()
-        conn.close()
-        # Destroying all the data and closing window
-        editor.destroy()
-
-    def exit_fun():
+    def exit():
         ask = messagebox.askyesno("exit", "Do you want to exit?")
         if ask == 1:
             root.destroy()
@@ -259,7 +249,7 @@ def login_page():
     root = Tk()
     root.title('Student Management System')
 
-    root.geometry('900x700')
+    root.geometry('900x600')
     root.iconbitmap("graduate.ico")
 
     # dataframe
@@ -272,48 +262,48 @@ def login_page():
     DataFrameLeft.place(x=0, y=5, width=500, height=300)
 
     # Create text boxes
-    name = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
-    name.grid(row=0, column=1, pady=5)
+    Name_entry = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
+    Name_entry.grid(row=0, column=1, pady=5)
 
-    fathers_name = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
-    fathers_name.grid(row=1, column=1, pady=5)
+    FathersName_entry = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
+    FathersName_entry.grid(row=1, column=1, pady=5)
 
-    mothers_name = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
-    mothers_name.grid(row=2, column=1, pady=5)
+    MothersName_entry = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
+    MothersName_entry.grid(row=2, column=1, pady=5)
 
-    address = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
-    address.grid(row=3, column=1, pady=5)
+    Address_entry = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
+    Address_entry.grid(row=3, column=1, pady=5)
 
-    gender = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
-    gender.grid(row=3, column=1, pady=5)
+    Gender_entry = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
+    Gender_entry.grid(row=3, column=1, pady=5)
 
-    date_of_birth = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
-    date_of_birth.grid(row=4, column=1, pady=5)
+    DOB_entry = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
+    DOB_entry.grid(row=4, column=1, pady=5)
 
-    phone_num = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
-    phone_num.grid(row=17, column=1, pady=5)
+    PhoneNum_entry = Entry(DataFrameLeft, font=("Times", 15, "bold"), bg="white", width=30)
+    PhoneNum_entry.grid(row=17, column=1, pady=5)
 
     # Create textbox labels
-    name_label = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Students Name")
-    name_label.grid(row=0, column=0, pady=5)
+    Name =Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Students Name")
+    Name.grid(row=0, column=0, pady=5)
 
-    fathers_name_label = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Fathers Name")
-    fathers_name_label.grid(row=1, column=0, pady=5)
+    Fathers= Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Fathers Name")
+    Fathers.grid(row=1, column=0, pady=5)
 
-    mothers_name_label = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Mothers Name")
-    mothers_name_label.grid(row=2, column=0, pady=5)
+    Mothers = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Mothers Name")
+    Mothers.grid(row=2, column=0, pady=5)
 
-    address_label = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Adress")
-    address_label.grid(row=3, column=0, pady=5)
+    Address = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Adress")
+    Address.grid(row=3, column=0, pady=5)
 
-    gender_label = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Gender")
-    gender_label.grid(row=3, column=0, pady=5)
+    Gender = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Gender")
+    Gender.grid(row=3, column=0, pady=5)
 
-    date_of_birth_label = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="DOB")
-    date_of_birth_label.grid(row=4, column=0, pady=5)
+    DOB = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="DOB")
+    DOB.grid(row=4, column=0, pady=5)
 
-    phone_num_label = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Phone No")
-    phone_num_label.grid(row=17, column=0, pady=5)
+    PhoneNum = Label(DataFrameLeft, font=("Times", 15, "bold"), bg="lightblue", text="Phone No")
+    PhoneNum.grid(row=17, column=0, pady=5)
 
     # Data Frame Right
     DataFrameRight = LabelFrame(DataFrame, bd=10, bg="lightblue", relief=RIDGE, padx=18,
@@ -321,11 +311,11 @@ def login_page():
     DataFrameRight.place(x=500, y=5, width=300, height=300)
     font = ("Times", 10, "bold")
 
-    delete_box_label = Label(DataFrameRight, font=("Times", 12, "bold"), bg="lightblue", text="ID")
-    delete_box_label.grid(row=3, column=0, pady=5)
+    clear_box_label = Label(DataFrameRight, font=("Times", 12, "bold"), bg="lightblue", text="ID")
+    clear_box_label.grid(row=3, column=0, pady=5)
 
-    delete_box = Entry(DataFrameRight, font=("Times", 10, "bold"), bg="white", width=30)
-    delete_box.grid(row=3, column=1, pady=5)
+    clear_box = Entry(DataFrameRight, font=("Times", 10, "bold"), bg="white", width=30)
+    clear_box.grid(row=3, column=1, pady=5)
 
     # Create submit button
 
@@ -345,7 +335,7 @@ def login_page():
     edit_btn = Button(DataFrameRight, font=("Times", 12, "bold"), bg="lightblue", text="Update", command=edit)
     edit_btn.grid(row=4, column=1, padx=7, pady=5)
 
-    exit_btn = Button(DataFrameRight, font=("Times", 12, "bold"), bg="lightblue", text="Exit", command=exit_fun)
+    exit_btn = Button(DataFrameRight, font=("Times", 12, "bold"), bg="lightblue", text="Exit", command=exit)
     exit_btn.grid(row=6, column=1, ipadx=10, padx=7, pady=5)
 
     details_frame = LabelFrame(root, bd=10, relief=RIDGE, text="Students Records", font=("Times", 10, "bold"))
